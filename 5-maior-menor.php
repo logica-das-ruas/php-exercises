4d6c3a5e21c4bba6d6376a775d705f94c6c9e635
marcos012 <marcospaulo.ad@gmail.com>
2019-09-07 00:00:08 -0300

    feito exercicio que verifica qual o maior e o menor numero de um array

---
+++ 5-maior-menor.php
@@ -0,0 +1,6 @@
+<?php 
+    //  5) Ler um array com 10 inteiros e imprimir o maior e o menor elemento.
+
+    $intArray = array(7, 2, 4, 6, 10, 99, 5,4, 6, 43);
+    echo "maior " . max($intArray) . "<br>" . "menor " . min($intArray);
+?>
